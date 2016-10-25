a0cfc7cc7f263f0212d2778aeee7975a96208ba7
Denis Sadilo <Denis Sadilo>
2016-10-26 00:34:19 +0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = (code, instance, method) => {
   return new Promise((resolve, reject) => {
-    var spawn = require('child_process').spawn
+      'use strict';
+    const spawn = require('child_process').spawn
     const ls = spawn('./codeAnalys/CodeAnalys.exe', [instance, code, method])
     let output = ''
     ls.stdout.on('data', (data) => {
